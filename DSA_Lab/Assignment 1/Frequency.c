79fdfacbc8cbbf1013ae2422f586bd1a698a542f
Subhranil Nandy <69036670+Subhranil2004@users.noreply.github.com>
2023-08-05 20:55:47 +0530

    Update Frequency.c

--- DSA_Lab/Assignment 1/Frequency.c
+++ DSA_Lab/Assignment 1/Frequency.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 int frequency(FILE *, char);
+void print_contents(FILE *);
 int main()
 {
     FILE *fptr;
     char fpath[100];
     char ch;
-    printf("Enter the relative filepath : ");
+    printf("Enter the relative filepath : "); 
+    // For testing with the sample provided just type "Sample.txt" without quotes.
     scanf("%s", fpath);
     fptr = fopen(fpath, "r");
     if (fptr == NULL)
@@ -17,13 +19,28 @@ int main()
         exit(0);
     }
 
-    printf("Enter the character : ");
+    printf("Contents of the file :\n");
+    print_contents(fptr);
+    rewind(fptr); 
+    // rewind() is used to move the file pointer to the beginning of the file stream.
+    // fseek(fptr, 0L, SEEK_SET); performs the same task.
+    printf("\nEnter the character : ");
     ch = getchar();
     while (ch == ' ' || ch == '\n' || ch == '\t')
         ch = getchar();
     printf("The frequency of character \'%c\' = %d.\n", ch, frequency(fptr, ch));
     fclose(fptr);
     return 0;
+}
+void print_contents(FILE *fp)
+{
+    char readch;
+    readch = fgetc(fp);
+    while(readch != EOF)
+    {
+        printf("%c", readch);
+        readch = fgetc(fp);
+    }
 }
 int frequency(FILE *fp, char ch)
 {
